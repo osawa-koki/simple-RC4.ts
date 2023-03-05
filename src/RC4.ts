3b5fc6fcaf0c1b4b87367897e0a18545c72e636f
osawa-koki <github@osawa.dev>
2023-03-05 23:34:07 +0900

    RC4実装した。

---
+++ src/RC4.ts
@@ -0,0 +1,50 @@
+
+function KSA(key: number[]): number[] {
+  const S = new Array(256);
+  for (let i = 0; i < 256; i++) {
+    S[i] = i;
+  }
+  let j = 0;
+  for (let i = 0; i < 256; i++) {
+    j = (j + S[i] + key[i % key.length]) % 256;
+    [S[i], S[j]] = [S[j], S[i]];
+  }
+  return S;
+}
+
+function PRGA(S: number[]): number[] {
+  const K = [];
+  let i = 0;
+  let j = 0;
+  while (K.length < 256) {
+    i = (i + 1) % 256;
+    j = (j + S[i]) % 256;
+    [S[i], S[j]] = [S[j], S[i]];
+    K.push(S[(S[i] + S[j]) % 256]);
+  }
+  return K;
+}
+
+export function encrypt(data: string, key: string): string {
+  const _data = new Uint8Array(new TextEncoder().encode(data));
+  const _key = new Uint8Array(new TextEncoder().encode(key));
+  const S = KSA(Array.from(_key));
+  const gen = PRGA(Array.from(S));
+  const result = new Uint8Array(_data.length);
+  for (let i = 0; i < _data.length; i++) {
+    result[i] = _data[i] ^ gen[i];
+  }
+  return btoa(String.fromCharCode(...result));
+}
+
+export function decrypt(data: string, key: string): string {
+  const _data = new Uint8Array(atob(data).split('').map(char => char.charCodeAt(0)));
+  const _key = new Uint8Array(new TextEncoder().encode(key));
+  const S = KSA(Array.from(_key));
+  const gen = PRGA(Array.from(S));
+  const result = new Uint8Array(_data.length);
+  for (let i = 0; i < _data.length; i++) {
+    result[i] = _data[i] ^ gen[i];
+  }
+  return new TextDecoder().decode(result);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import { encrypt, decrypt } from './RC4';
+
 console.log("Hello, TypeScript!");
+
+const data = 'Hello, world!';
+const key = 'my_secret_key';
+const encrypted = encrypt(data, key);
+console.log(encrypted);
+const decrypted = decrypt(encrypted, key);
+console.log(decrypted);
