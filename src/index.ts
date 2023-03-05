84e0e4ede341907ca2343ee3e3cf748bf6a40494
osawa-koki <github@osawa.dev>
2023-03-05 23:41:55 +0900

    コマンドラインの解析まで。

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+import yargs, { Arguments } from 'yargs';
 import { encrypt, decrypt } from './RC4';
 
-console.log("Hello, TypeScript!");
+(async () => {
+  const argv: Arguments = await yargs(process.argv)
+    .option('k', {
+      alias: 'key',
+      describe: '暗号化キー',
+      demandOption: true,
+      type: 'string',
+    })
+    .option('m', {
+      alias: 'message',
+      describe: '暗号化するメッセージ',
+      demandOption: true,
+      type: 'string',
+    })
+    .argv;
 
-const data = 'Hello, world!';
-const key = 'my_secret_key';
-const encrypted = encrypt(data, key);
-console.log(encrypted);
-const decrypted = decrypt(encrypted, key);
-console.log(decrypted);
+  const key: string = argv.key as string;
+  const message: string = argv.message as string;
+
+  console.log(`key: ${key}`);
+  console.log(`message: ${message}`);
+
+  const encryptResult: string = encrypt(message, key);
+  const decryptResult: string = decrypt(encryptResult, key);
+
+  console.log(`encrypt: ${encryptResult}`);
+  console.log(`decrypt: ${decryptResult}`);
+
+})();
